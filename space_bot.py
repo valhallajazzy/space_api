729f413f0d95221bc3ee911569c86704fa06df92
valhallajazzy <valhallajazzy@mail.ru>
2023-12-13 23:56:55 +0600

    Add telegram-bot service

--- space_bot.py
+++ space_bot.py
@@ -1,14 +1,23 @@
+import os
+import random
+from time import sleep
+
 import telegram
 from telegram import InputMediaPhoto
-import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-bot = telegram.Bot(token=f'{os.environ["TELEGRAM_TOKEN"]}')
-updates = bot.get_updates()
+def main():
+    load_dotenv()
+    bot = telegram.Bot(token=f'{os.environ["TELEGRAM_TOKEN"]}')
+    images = [i[2] for i in os.walk('./images/')][0]
+    while True:
+        random.shuffle(images)
+        for image in images:
+            media = InputMediaPhoto(media=open(f'./images/{image}', 'rb'))
+            bot.send_media_group(chat_id='@spaceimg_channel', media=[media])
+            sleep(os.getenv("DRINKS_PATH", default=14400))
 
-print(bot.get_me())
-bot.send_message(chat_id='@spaceimg_channel', text='Привет, как дела?')
-media_1 = InputMediaPhoto(media=open('./images/earth_1.png', 'rb'))
-bot.send_media_group(chat_id='@spaceimg_channel', media=[media_1])
+
+if __name__ == '__main__':
+    main()
